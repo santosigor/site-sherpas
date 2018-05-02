70724bb069789f496b0e1794ac9976fbf0f70317
Igor dos Santos <igor.santos@fiap.com.br>
2018-05-02 18:24:46 -0300

    dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,29 @@ $(document).ready(function() {
 
   
 });
+
+$('.carousel-nossos-cursos').slick({
+  centerMode: true,
+  //centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
